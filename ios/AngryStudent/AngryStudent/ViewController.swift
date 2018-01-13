e3c7aed3e37fbf69335f39148d12e5fbcedcb969
Mateusz Orzoł <mateusz.orzol@proexe.pl>
2018-01-13 13:02:19 +0100

    Add IndoorwaySDK

--- ios/AngryStudent/AngryStudent/ViewController.swift
+++ ios/AngryStudent/AngryStudent/ViewController.swift
@@ -7,19 +7,40 @@
 //
 
 import UIKit
+import IndoorwaySdk
 
 class ViewController: UIViewController {
+    
+    let des = IndoorwayMapDescription(buildingUuid: "CScrSxCVhQg", mapUuid: "3-_M01M3r5w")
+    
+    private var mapView: IndoorwayMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView = IndoorwayMapView()
+        self.view.addSubview(mapView)
+        mapView.frame = view.frame
+        
+        configureMap()
+        mapView.delegate = self
+    }
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
+    private func configureMap() {
+        mapView.loadMap(with: des) { [weak self] (completed) in
+            self?.mapView.showsUserLocation = completed
+        }
+    }
+    
+}
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
+extension ViewController: IndoorwayMapViewDelegate {
+    
+    func mapViewDidFinishLoadingMap(_ mapView: IndoorwayMapView) {
+        print("Załadowało")
+    }
+    
+    func mapViewDidFailLoadingMap(_ mapView: IndoorwayMapView, withError error: Error) {
+        print("Nie załadowało ††††")
+    }
 }
 
