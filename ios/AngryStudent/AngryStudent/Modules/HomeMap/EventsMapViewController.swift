51225d1c805b8e425c6dd05ceedb7331ee74ba1f
Mateusz Orzoł <mateusz.orzol@proexe.pl>
2018-01-13 22:38:20 +0100

    Add Owner view

--- ios/AngryStudent/AngryStudent/Modules/HomeMap/EventsMapViewController.swift
+++ ios/AngryStudent/AngryStudent/Modules/HomeMap/EventsMapViewController.swift
@@ -1,79 +1,126 @@
 import UIKit
 import IndoorwaySdk
-
-
+import RxSwift
 
 class EventsMapViewController: BasicViewController, IndoorwayMapListener, IndoorwayPositionListener {
-  @IBOutlet private weak var eventsMapView: EventsMapView! {
-    didSet {
-      updateMap()
+    @IBOutlet private weak var eventsMapView: EventsMapView! {
+        didSet {
+            updateMap()
+        }
     }
-  }
-  
-  
-  private var mapSyncing: Bool = false
-  private var positionSyncing: Bool = false
-  
-  private var currentMapDesc: IndoorwayMapDescription? {
-    didSet {
-      if currentMapDesc?.mapUuid != oldValue?.buildingUuid {
-        updateMap()
-      }
-      
-    }
-  }
-  
-  deinit {
-    if mapSyncing {
-      IndoorwayLocationSdk.instance().map.onChange.removeListener(listener: self)
-    }
-    if positionSyncing {
-      IndoorwayLocationSdk.instance().position.onChange.removeListener(listener: self)
-    }
-  }
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    IndoorwayLocationSdk.instance().position.onChange.addListener(listener: self)
-    positionSyncing = true
-    IndoorwayLocationSdk.instance().map.onChange.addListener(listener: self)
-    mapSyncing = true
-    currentMapDesc = IndoorwayLocationSdk.instance().map.latest()
-    isLoading = true
-  }
-  
-  
-  private var x: IndoorwayLocation? {
-    didSet {
-      if oldValue == nil && x != nil {
-        eventsMapView!.chuj(x!)
-      }
-    }
-  }
-  
-  // MARK: IndoorwayPositionListener
-  func positionChanged(position: IndoorwayLocation) {
     
-    x = position
-  }
-
-  
-  // MARK: IndoorwayMapListener
-  func mapChanged(map: IndoorwayMapDescription) {
+    private let ownerViewModel = OwnerVotesViewModel()
+    private let disposeBag = DisposeBag()
     
-    currentMapDesc = map
-  }
-  
-  // MARK: private
-  private func updateMap() {
-    if let mapDesc: IndoorwayMapDescription = currentMapDesc {
-      
-      isLoading = eventsMapView != nil
-      eventsMapView?.load(with: mapDesc) {
-        [weak self] (succed: Bool) in
-        self?.isLoading = false
-      }
+    private var mapSyncing: Bool = false
+    private var positionSyncing: Bool = false
+    
+    private var currentMapDesc: IndoorwayMapDescription? {
+        didSet {
+            if currentMapDesc?.mapUuid != oldValue?.buildingUuid {
+                updateMap()
+            }
+        }
     }
-  }
-  
+    
+    let ownerView: OwnerVotesView = {
+        $0.alpha = 0
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(OwnerVotesView())
+    
+    deinit {
+        if mapSyncing {
+            IndoorwayLocationSdk.instance().map.onChange.removeListener(listener: self)
+        }
+        if positionSyncing {
+            IndoorwayLocationSdk.instance().position.onChange.removeListener(listener: self)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        IndoorwayLocationSdk.instance().position.onChange.addListener(listener: self)
+        positionSyncing = true
+        IndoorwayLocationSdk.instance().map.onChange.addListener(listener: self)
+        mapSyncing = true
+        currentMapDesc = IndoorwayLocationSdk.instance().map.latest()
+        isLoading = true
+        setupOwnerView()
+        addOwnerView()
+    }
+    
+    
+    private var x: IndoorwayLocation? {
+        didSet {
+            if oldValue == nil && x != nil {
+                eventsMapView!.chuj(x!)
+            }
+        }
+    }
+    
+    // MARK: IndoorwayPositionListener
+    func positionChanged(position: IndoorwayLocation) {
+        
+        x = position
+    }
+    
+    
+    // MARK: IndoorwayMapListener
+    func mapChanged(map: IndoorwayMapDescription) {
+        
+        currentMapDesc = map
+    }
+    
+    // MARK: private
+    private func updateMap() {
+        if let mapDesc: IndoorwayMapDescription = currentMapDesc {
+            
+            isLoading = eventsMapView != nil
+            eventsMapView?.load(with: mapDesc) {
+                [weak self] (succed: Bool) in
+                self?.isLoading = false
+            }
+        }
+    }
+    
+    // MARK: - Owner View
+    
+    // MARK: - Binding
+    
+    private func bindView() {
+        ownerViewModel.groupEvent.asObservable().subscribe(onNext: { [weak self] (event) in
+            guard let event = event else { return }
+            self?.ownerView.setup(model: event)
+            self?.navigationController?.title = event.name
+        }).disposed(by: disposeBag)
+    }
+    
+    // MARK: - UI
+    
+    private func removeOwnerView() {
+        UIView.animate(withDuration: 0.5) { [weak self] in
+            self?.ownerView.alpha = 0
+        }
+    }
+    
+    private func addOwnerView() {
+        UIView.animate(withDuration: 0.5) { [weak self] in
+            self?.ownerView.alpha = 1
+        }
+    }
+    
+    private func setupOwnerView() {
+        self.view.addSubview(ownerView)
+        setupOwnerViewConstrains()
+        bindView()
+    }
+    
+    private func setupOwnerViewConstrains() {
+        ownerView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 10).isActive = true
+        ownerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        ownerView.widthAnchor.constraint(equalToConstant: 230).isActive = true
+        ownerView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+    }
+    
 }
---
+++ ios/AngryStudent/AngryStudent/Modules/OwnerVotes/OwnerVotesView.swift
@@ -0,0 +1,258 @@
+//
+//  OwnerVotesView.swift
+//  AngryStudent
+//
+//  Created by Mateusz Orzoł on 13.01.2018.
+//  Copyright © 2018 Paweł Czerwiński. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class  OwnerVotesView: BasicView  {
+    
+    // MARK: - Delegate
+    
+    // MARK: - Properties
+    
+    // MARK: - Outlets
+    
+    private let bcgView: UIView = {
+        $0.backgroundColor = Color.white
+        $0.layer.cornerRadius = 7
+        $0.alpha = 0.85
+        $0.clipsToBounds = true
+        $0.contentMode = .scaleAspectFit
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIImageView())
+    
+    private let evnetImageView: UIImageView = {
+        $0.backgroundColor = Color.clear
+        $0.tintColor = Color.blue
+        $0.clipsToBounds = true
+        $0.contentMode = .scaleAspectFit
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIImageView())
+    
+    private let desLabel: UILabel = {
+        $0.textColor = Color.blueDark
+        $0.font = UIFont.systemFont(ofSize: 20)
+        $0.numberOfLines = 1
+        $0.textAlignment = .center
+        $0.lineBreakMode = .byTruncatingTail
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UILabel())
+    
+    private let topStackView: UIStackView = {
+        $0.axis = .horizontal
+        $0.distribution = .fill
+        $0.alignment = .center
+        $0.spacing = 5
+        $0.isUserInteractionEnabled = true
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIStackView())
+    
+    private let iconImageView: UIImageView = {
+        $0.backgroundColor = Color.clear
+        $0.image = #imageLiteral(resourceName: "Users")
+        $0.tintColor = Color.blueDark
+        $0.clipsToBounds = true
+        $0.contentMode = .scaleAspectFit
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIImageView())
+    
+    private let nrLabel: UILabel = {
+        $0.textColor = Color.blue
+        $0.font = UIFont.systemFont(ofSize: 50)
+        $0.numberOfLines = 1
+        $0.textAlignment = .left
+        $0.lineBreakMode = .byTruncatingTail
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UILabel())
+    
+    private let viewsStackView: UIStackView = {
+        $0.axis = .horizontal
+        $0.distribution = .fill
+        $0.alignment = .center
+        $0.spacing = 5
+        $0.isUserInteractionEnabled = true
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIStackView())
+    
+    private let infoLabel: UILabel = {
+        $0.textColor = Color.blueDark
+        $0.font = UIFont.systemFont(ofSize: 45)
+        $0.text = "VOTES"
+        $0.numberOfLines = 1
+        $0.textAlignment = .left
+        $0.lineBreakMode = .byTruncatingTail
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UILabel())
+    
+    private let okImageView: UIImageView = {
+        $0.backgroundColor = Color.clear
+        $0.image = #imageLiteral(resourceName: "Up")
+        $0.tintColor = Color.black
+        $0.clipsToBounds = true
+        $0.contentMode = .scaleAspectFit
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIImageView())
+    
+    private let okLabel: UILabel = {
+        $0.textColor = Color.green
+        $0.font = UIFont.systemFont(ofSize: 40)
+        $0.numberOfLines = 1
+        $0.textAlignment = .left
+        $0.lineBreakMode = .byTruncatingTail
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UILabel())
+    
+    private let okStackView: UIStackView = {
+        $0.axis = .horizontal
+        $0.distribution = .fill
+        $0.alignment = .center
+        $0.spacing = 5
+        $0.isUserInteractionEnabled = true
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIStackView())
+    
+    private let badImageView: UIImageView = {
+        $0.backgroundColor = Color.clear
+        $0.image = #imageLiteral(resourceName: "down")
+        $0.tintColor = Color.black
+        $0.clipsToBounds = true
+        $0.contentMode = .scaleAspectFit
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIImageView())
+    
+    private let badLabel: UILabel = {
+        $0.textColor = Color.red
+        $0.font = UIFont.systemFont(ofSize: 40)
+        $0.numberOfLines = 1
+        $0.textAlignment = .left
+        $0.lineBreakMode = .byTruncatingTail
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UILabel())
+    
+    private let badStackView: UIStackView = {
+        $0.axis = .horizontal
+        $0.distribution = .fill
+        $0.alignment = .center
+        $0.spacing = 5
+        $0.isUserInteractionEnabled = true
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIStackView())
+    
+    private let votesStackView: UIStackView = {
+        $0.axis = .horizontal
+        $0.distribution = .fill
+        $0.alignment = .fill
+        $0.spacing = 35
+        $0.isUserInteractionEnabled = true
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIStackView())
+    
+    // MARK: - Inititalization
+    
+    override public func initialize() {
+        super.initialize()
+        setupUI()
+    }
+    
+    // MARK: - Actions
+    
+    // MARK: - Helpers
+    
+    func setup(model: GroupEvent) {
+        
+        if let iconName = model.iconName {
+            evnetImageView.image = UIImage(named: iconName)
+        }
+        desLabel.text = model.name
+        nrLabel.text = "\(model.viewersNum!)"
+        okLabel.text = "\(model.votes!.ok!)"
+        badLabel.text = "\(model.votes!.bad.count)"
+    }
+    
+    // MARK: - Setup
+    
+    private func setupUI() {
+        self.backgroundColor = Color.clear
+        addSubview(bcgView)
+        topStackView.addArrangedSubview(evnetImageView)
+        topStackView.addArrangedSubview(desLabel)
+        //viewsStackView.addArrangedSubview(nrLabel)
+        //viewsStackView.addArrangedSubview(iconImageView)
+        addSubview(topStackView)
+        //addSubview(viewsStackView)
+        //addSubview(infoLabel)
+        okStackView.addArrangedSubview(okImageView)
+        okStackView.addArrangedSubview(okLabel)
+        badStackView.addArrangedSubview(badImageView)
+        badStackView.addArrangedSubview(badLabel)
+        votesStackView.addArrangedSubview(okStackView)
+        votesStackView.addArrangedSubview(badStackView)
+        addSubview(votesStackView)
+        setupBcgView()
+        setupEventImageView()
+        setupOkBadImageVIews()
+//        setupIconImageView()
+        setupTopStackView()
+        //setupViewsStackView()
+        setupVotesStackView()
+    }
+    
+    private func setupBcgView() {
+        bcgView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        bcgView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        bcgView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        bcgView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+    }
+    
+    private func setupEventImageView() {
+        evnetImageView.heightAnchor.constraint(equalToConstant: 35).isActive = true
+        evnetImageView.heightAnchor.constraint(equalTo: evnetImageView.widthAnchor).isActive = true
+    }
+    
+//    private func setupIconImageView() {
+//        iconImageView.heightAnchor.constraint(equalToConstant: 60).isActive = true
+//        iconImageView.heightAnchor.constraint(equalTo: evnetImageView.widthAnchor).isActive = true
+//    }
+    
+    private func setupOkBadImageVIews() {
+        okImageView.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        okImageView.heightAnchor.constraint(equalTo: okImageView.widthAnchor).isActive = true
+        badImageView.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        badImageView.heightAnchor.constraint(equalTo: badImageView.widthAnchor).isActive = true
+    }
+    
+    private func setupTopStackView() {
+        topStackView.topAnchor.constraint(equalTo: topAnchor, constant: 10).isActive = true
+        topStackView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+    }
+    
+//    private func setupViewsStackView() {
+//        viewsStackView.topAnchor.constraint(equalTo: topStackView.bottomAnchor, constant: 10).isActive = true
+//        viewsStackView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+//    }
+    
+    private func setupVotesStackView() {
+        votesStackView.topAnchor.constraint(equalTo: topStackView.bottomAnchor, constant: 15).isActive = true
+        votesStackView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+    }
+}
